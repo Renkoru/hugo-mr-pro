00ff4d91754ce5be24e68dd3e5e272769ee73a0b
Maxim Rurenko <mrurenko@gmail.com>
2022-05-05 20:36:07 +0700

    add init implementation

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    "content/**/*.md",
+    "layouts/**/*.html",
+    "../../content/**/*.md",
+    "../../layouts/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
